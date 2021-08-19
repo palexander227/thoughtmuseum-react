f8f55ca00a9a97ae87a293722796e6c144571281
ishtiyakzafar <zaferistak@gmail.com>
2021-08-19 17:52:33 +0530

    new update

--- src/service/http.js
+++ src/service/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const appServiceName = "https://thought-museum.herokuapp.com";
+const appServiceName = "https://thoughtmuseum-api.herokuapp.com";
 
 class RestfulProvider {
   constructor() {
